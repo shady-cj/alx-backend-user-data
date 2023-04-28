797a1481e8eed0242318315663550833125c175b
peter <petersp2000@gmail.com>
2023-04-28 10:28:45 +0100

    Implemented log formater

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -32,10 +32,19 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: typing.List[str]):
+        """
+        Initializes the formatter with the required fields
+        and calling the super() constructor to configure the
+        FORMAT fields.
+        """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """
+        Formats the record message based on the REDACTION, and FIELDs
+        set in the constructor and returns the formatted log Record
+        """
         formattedMessage = filter_datum(self.fields, self.REDACTION,
                                         record.getMessage(), self.SEPARATOR)
         record.msg = formattedMessage
