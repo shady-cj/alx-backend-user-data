88b2ac0cc281677a6edc4455b55c7c91dd845c21
peter <petersp2000@gmail.com>
2023-05-03 20:21:29 +0100

    implemented basic auth configuration

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Implementing an authentication system
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Handlers that defines if auth is required or not.
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        auth header
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Returns the current user.
+        """
+        return None
