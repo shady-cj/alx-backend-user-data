ee087f2f39c8e8ee815e79fc5f15deef204d2098
peter <petersp2000@gmail.com>
2023-05-04 13:50:12 +0100

    created a starter class to implement a session authentication system

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Creating a class to handle session authentication.
+"""
+from .auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    Extending Auth and implementing a session auth system.
+    """
+    pass
