3dddf9dce9ea767634dd6c989918e69f58169c90
peter <petersp2000@gmail.com>
2023-05-04 14:29:54 +0100

    Retrieving a certain user from the cookie set with session_id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,7 @@ Creating a class to handle session authentication.
 """
 from .auth import Auth
 import uuid
+from models.users import User
 
 
 class SessionAuth(Auth):
@@ -31,3 +32,11 @@ class SessionAuth(Auth):
         if session_id is None or type(session_id) != str:
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """
+        returns a User instance based on a cookie value:
+        """
+        get_cookie = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(get_cookie)
+        return User.get(user_id)
