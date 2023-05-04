4d9bab4fcfb14c2dc48c7dd1e85de00d316eb73e
peter <petersp2000@gmail.com>
2023-05-04 14:23:51 +0100

    excluded login route from paths that requires authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -61,12 +61,13 @@ def before_request_handler():
                              [
                                 "/api/v1/status/",
                                 "/api/v1/unauthorized/",
-                                "/api/v1/forbidden/"
+                                "/api/v1/forbidden/",
+                                "/api/v1/auth_session/login/"
                               ]):
         return None
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request) is None and\
+       auth.session_cookie(request) is None:
         abort(401)
-
     if auth.current_user(request) is None:
         abort(403)
     request.current_user = auth.current_user(request)
