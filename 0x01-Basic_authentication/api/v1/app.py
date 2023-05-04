51f705afce065c247aa068b10ed605574a2828f7
peter <petersp2000@gmail.com>
2023-05-04 05:59:29 +0100

    implementation basic authorization by adding a new class BasicAuth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -22,6 +22,7 @@ elif getenv('AUTH_TYPE') == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
