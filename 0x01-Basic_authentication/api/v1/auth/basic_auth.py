e33ddadedec49e1045e55bec8efcdf81ec249020
peter <petersp2000@gmail.com>
2023-05-04 06:30:51 +0100

    implementing method for extracting the token from the authorization header and decoding from base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 Implementing a basic authentication system
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -21,4 +22,21 @@ class BasicAuth(Auth):
             if authorization_header.startswith('Basic '):
                 return authorization_header.lstrip('Basic ')
 
+        return None
+
+    def decode_base64_authorization_header(
+                                           self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """
+        extracts and returns the decoded auth token
+        """
+        if type(base64_authorization_header) == str and\
+           base64_authorization_header is not None:
+            token = base64_authorization_header
+            try:
+                decoded_token = base64.b64decode(token)
+                return decoded_token.decode('utf-8')
+            except Exception as e:
+                return None
         return None
