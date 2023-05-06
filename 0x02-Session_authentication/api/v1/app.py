d385fd5919a4f57572cdaad0d47a4542463498df
peter <petersp2000@gmail.com>
2023-05-06 11:12:58 +0100

    Final linting

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -34,6 +34,7 @@ elif getenv('AUTH_TYPE') == "session_db_auth":
     from api.v1.auth.session_db_auth import SessionDBAuth
     auth = SessionDBAuth()
 
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
