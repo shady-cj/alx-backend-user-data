45e2a1428886ecb440e3d5da3e78f34ed1b6cc59
peter <petersp2000@gmail.com>
2023-05-21 16:27:45 +0100

    creating a db storage to add users

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """DB module
 """
 from sqlalchemy import create_engine
@@ -29,7 +30,7 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
-    
+
     def add_user(self, email, hashed_password) -> User:
         """
         Adds and returns the user object
