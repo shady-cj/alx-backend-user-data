08c4f11a866ef79ab2d25ebfe8dda871bdd29ad0
peter <petersp2000@gmail.com>
2023-05-21 20:54:23 +0100

    feat: added a new method get_user_from_session_id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -71,7 +71,8 @@ class Auth:
             return None
 
     def get_user_from_session_id(self,
-                                 session_id: Optional[str] = None) -> Optional[User]:
+                                 session_id: Optional[str] = None
+                                 ) -> Optional[User]:
         """
         retrieves user from session_id
         """
