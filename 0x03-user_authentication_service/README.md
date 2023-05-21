fb92c9bcbe4621b8e11af0b69f9bfd8eced49425
peter <petersp2000@gmail.com>
2023-05-21 21:21:08 +0100

    feat: logout functionality added

--- 0x03-user_authentication_service/README.md
+++ 0x03-user_authentication_service/README.md
@@ -574,3 +574,12 @@ In this task, you will implement Auth.destroy_session. The method takes a single
 The method updates the corresponding user’s session ID to None.
 
 Remember to only use public methods of self._db.
+
+
+### 14. Log out
+
+In this task, you will implement a logout function to respond to the DELETE /sessions route.
+
+The request is expected to contain the session ID as a cookie with key "session_id".
+
+Find the user with the requested session ID. If the user exists destroy the session and redirect the user to GET /. If the user does not exist, respond with a 403 HTTP status.
--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,7 +3,7 @@
 Creating a flask application to serve
 the authentication service
 """
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 app = Flask(__name__)
@@ -47,6 +47,19 @@ def login():
         return resp
     else:
         abort(401)
+
+
+@app.route("/sessions", methods=['DELETE'])
+def logout():
+    """
+    Handles logout functionality
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect('/', code=302)
 
 
 if __name__ == "__main__":
