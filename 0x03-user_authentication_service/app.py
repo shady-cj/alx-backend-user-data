8b6374b3d010bfc296cd2d1d79259219574897aa
peter <petersp2000@gmail.com>
2023-05-21 21:48:33 +0100

    feat: added the get_reset_password_token view

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -74,5 +74,18 @@ def profile():
     return jsonify({"email": user.email})
 
 
+@app.route('/reset_password', methods=["POST"])
+def get_reset_password_token():
+    """
+    handles the reset password token view
+    """
+    email = request.form.get('email')
+    try:
+        token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": token})
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5002")
