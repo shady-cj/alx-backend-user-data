7bb764d6f44db80796175134e1f6046c0a83bb5a
peter <petersp2000@gmail.com>
2023-05-21 18:44:19 +0100

    feat: added new method create_session to the auth class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -56,3 +56,15 @@ class Auth:
             return bcrypt.checkpw(password.encode(), user.hashed_password)
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """
+        Creates a session id for an email
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            uid = _generate_uuid()
+            self._db.update_user(user.id, session_id=uid)
+            return uid
+        except NoResultFound:
+            return None
