7778b0fa0f5cb506bfc495ac2db1c015b2fd797c
peter <petersp2000@gmail.com>
2023-05-21 21:34:32 +0100

    feat: added the profile page

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -62,5 +62,17 @@ def logout():
     return redirect('/', code=302)
 
 
+@app.route('/profile', methods=["GET"])
+def profile():
+    """
+    gets a user profile
+    """
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    return jsonify({"email": user.email})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5002")
