70e3096232c8250f10fbe301e0fb9c4fcabf1299
peter <petersp2000@gmail.com>
2023-05-21 18:32:12 +0100

    feat: added a new util function _generate_uuid private to the auth module

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -19,7 +19,7 @@ def _hash_password(password: str) -> bytes:
     return hashed
 
 
-def _generate_uuid():
+def _generate_uuid() -> uuid.uuid4:
     """
     The function should return a string representation of a new UUID.
     """
